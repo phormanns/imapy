2f305053f224c940c36c617c2c39c50e42ee45c1
Darestiet <hallo@darestiet.de>
2026-09-03 14:11:55 +0200

    recognize xhtml messages

--- src/main/java/de/jalin/imap/mime/MessageData.java
+++ src/main/java/de/jalin/imap/mime/MessageData.java
@@ -60,18 +60,20 @@ public class MessageData implements Serializable {
 
     public void setText(final String text) {
         if (text != null && text.length() > 29) {
-            final String textBeginning = text.substring(0, 29);
-            if (textBeginning.startsWith("<p")
-                    || textBeginning.startsWith("<span")
-                    || textBeginning.contains("<html")
-                    || textBeginning.contains("<div")
-                    || textBeginning.contains("<!DOCTYPE html")
-                    || textBeginning.contains("<!DOCTYPE HTML")) {
+            final String leadingText29 = text.substring(0, 29);
+            if (leadingText29.startsWith("<p")
+                    || leadingText29.startsWith("<span")
+                    || leadingText29.contains("<html")
+                    || leadingText29.contains("<div")) {
                 setHtmlText(text);
-            } else {
-                this.text = text;
+                return;
             }
-        } else {
+            final String leadingText59 = text.substring(0, 59);
+            if (leadingText59.contains("<!DOCTYPE html")
+                    || leadingText59.contains("<!DOCTYPE HTML")) {
+                setHtmlText(text);
+                return;
+            }
             this.text = text;
         }
     }
